8b3e6b379d6428661dbff806cccb698a0251ad91
Sam Gardner <stgardner4@tamu.edu>
2022-02-05 16:18:38 -0600

    *sigh* I'm getting too used to python

--- api/config.php
+++ api/config.php
@@ -1,6 +1,6 @@
 <?php
-$relativePathToMetadataDir = "" # Insert path, relative to the parent directory of this file, to the "metadata" directory of a hdwx-operational output folder.
+$relativePathToMetadataDir = ""; # Insert path, relative to the parent directory of this file, to the "metadata" directory of a hdwx-operational output folder.
 # If your hdwx-operational config.txt contains "/var/www/html/wx4stg/" as the value for "targetDir", then $relativePathToMetadataDir would be the relative path to /var/www/html/wx4stg/metadata
-$baseProductsURL = "" # Insert a link to where "targetDir" is hosted on the public internet.
+$baseProductsURL = ""; # Insert a link to where "targetDir" is hosted on the public internet.
 # If your hdwx-operational config.txt contains"targetDir" is "/var/www/html/wx4stg/", then $baseProductsURL is "https://<yourdomain>.com/wx4stg/", in my case, "http://weather-dev.geos.tamu.edu/wx4stg/"
 ?>
